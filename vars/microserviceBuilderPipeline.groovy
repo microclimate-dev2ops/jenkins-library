d7166a8eacdff9d5850232e1573e0d9aa6420986
Mark Nuttall <mnuttall@uk.ibm.com>
2017-07-11 14:22:49 +0100

    Add testing support

--- vars/microserviceBuilderPipeline.groovy
+++ vars/microserviceBuilderPipeline.groovy
@@ -20,11 +20,19 @@
 
     build = 'true' - any value other than 'true' == false
     deploy = 'true' - any value other than 'true' == false
+    test = 'true' - `mvn verify` is run if this value is `true` and a pom.xml exists
+    debug = 'false' - namespaces created during tests are deleted unless this value is set to 'true'
     deployBranch = 'master' - only builds from this branch are deployed
     namespace = 'targetNamespace' - deploys into Kubernetes targetNamespace.
       Default is to deploy into Jenkins' namespace.
 
 -------------------------*/
+
+import com.cloudbees.groovy.cps.NonCPS
+import java.io.File
+import java.util.UUID
+import groovy.json.JsonOutput;
+import groovy.json.JsonSlurperClassic;
 
 def call(body) {
   def config = [:]
@@ -34,7 +42,7 @@ def call(body) {
   body.delegate = config
   body()
 
-  print "microserviceBuilderPipeline: config = ${config}"
+  print "microserviceBuilderPipeline : config = ${config}"
 
   def image = config.image
   def maven = (config.mavenImage == null) ? 'maven:3.5.0-jdk-8' : config.mavenImage
@@ -47,22 +55,34 @@ def call(body) {
   def deploy = (config.deploy ?: System.getenv ("DEPLOY")).trim().toLowerCase() == 'true'
   def namespace = config.namespace ?: (System.getenv("NAMESPACE") ?: "").trim()
 
-  // Extra elvis here to cope with env var being absent until pipeline chart catches up
+  // 'deploy', 'test' and 'debug' options were all added later. Helm chart may not have the associated properties set. 
+  def test = (config.test ?: (System.getenv ("TEST") ?: "false").trim()).toLowerCase() == 'true'
+  def debug = (config.debug ?: (System.getenv ("DEBUG") ?: "false").trim()).toLowerCase() == 'true'
   def deployBranch = config.deployBranch ?: ((System.getenv("DEFAULT_DEPLOY_BRANCH") ?: "").trim() ?: 'master')
 
-  print "microserviceBuilderPipeline: registry=${registry} registrySecret=${registrySecret} build=${build} deploy=${deploy} deployBranch=${deployBranch} namespace=${namespace}"
+  print "microserviceBuilderPipeline: registry=${registry} registrySecret=${registrySecret} build=${build} \
+  deploy=${deploy} deployBranch=${deployBranch} test=${test} debug=${debug} namespace=${namespace}"
 
-  /* Only mount registry secret if it's present */
+  // We won't be able to get hold of registrySecret if Jenkins is running in a non-default namespace that is not the deployment namespace. 
+  // In that case we'll need the registrySecret to have been ported over, perhaps during pipeline install. 
+
+  // Only mount registry secret if it's present
   def volumes = [ hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock') ]
   if (registrySecret) {
     volumes += secretVolume(secretName: registrySecret, mountPath: '/root')
   }
   print "microserviceBuilderPipeline: volumes = ${volumes}"
 
-  podTemplate(
+  testNamespace = "testns-${env.BUILD_ID}-" + UUID.randomUUID()
+  print "testing against namespace " + testNamespace
+
+    podTemplate(
     label: 'msbPod',
     containers: [
-      containerTemplate(name: 'maven', image: maven, ttyEnabled: true, command: 'cat'),
+      containerTemplate(name: 'maven', image: maven, ttyEnabled: true, command: 'cat',
+        envVars: [
+          containerEnvVar(key: 'KUBERNETES_NAMESPACE', value: testNamespace)
+        ]),
       containerTemplate(name: 'docker', image: docker, command: 'cat', ttyEnabled: true,
         envVars: [
           containerEnvVar(key: 'DOCKER_API_VERSION', value: '1.23.0')
@@ -92,13 +112,44 @@ def call(body) {
           stage ('Docker Build') {
             container ('docker') {
               sh "docker build -t ${image}:${gitCommit} ."
-              if (registry) {
-                if (!registry.endsWith('/')) {
-                  registry = "${registry}/"
+              if (!registry.endsWith('/')) {
+                registry = "${registry}/"
+              }
+              sh "ln -s /root/.dockercfg /home/jenkins/.dockercfg"
+              sh "docker tag ${image}:${gitCommit} ${registry}${image}:${gitCommit}"
+              sh "docker push ${registry}${image}:${gitCommit}"
+            }
+          }
+        }
+      }
+
+      /* replace '${image}:latest' with '${registry}{image}:${gitcommit}' in manifests/*
+         We'll need this so that we can use manifests/ for test or deployment.
+         It's only a local change and not committed back to git. */
+      sh "find manifests -type f | xargs sed -i \'s|${image}:latest|${registry}${image}:${gitCommit}|g\'"
+
+      if (test && fileExists('pom.xml')) {
+        stage ('Verify') {
+          container ('kubectl') {
+            sh "kubectl create namespace ${testNamespace}"
+            sh "kubectl label namespace ${testNamespace} test=true"
+
+            giveRegistryAccessToNamespace (testNamespace, registrySecret)
+
+            sh "kubectl apply -f manifests --namespace ${testNamespace}"
+
+            
+          }
+          container ('maven') {
+            try {
+              sh "mvn -B verify"
+            } finally {
+              step([$class: 'JUnitResultArchiver', allowEmptyResults: true, testResults: '**/target/failsafe-reports/*.xml'])
+              step([$class: 'ArtifactArchiver', artifacts: '**/target/failsafe-reports/*.txt', allowEmptyArchive: true])
+              if (!debug) {
+                container ('kubectl') {
+                  sh "kubectl delete namespace ${testNamespace}"
                 }
-                sh "ln -s /root/.dockercfg /home/jenkins/.dockercfg"
-                sh "docker tag ${image}:${gitCommit} ${registry}${image}:${gitCommit}"
-                sh "docker push ${registry}${image}:${gitCommit}"
               }
             }
           }
@@ -108,8 +159,6 @@ def call(body) {
       if (deploy && env.BRANCH_NAME == deployBranch && fileExists('manifests')) {
         stage ('Deploy') {
           container ('kubectl') {
-            sh "find manifests -type f | xargs sed -i \'s|${image}:latest|${registry}${image}:${gitCommit}|g\'"
-
             def deployCommand = "kubectl apply -f manifests"
             if (namespace) {
               deployCommand += " --namespace ${namespace} "
@@ -120,4 +169,40 @@ def call(body) {
       }
     }
   }
+}
+
+/* 
+  We have a (temporary) namespace that we want to grant CfC registry access to. 
+  String namespace: target namespace
+  String registrySecret: secret in Jenkins' namespace to use
+
+  1. Port registrySecret into namespace
+  2. Modify 'default' serviceaccount to use ported registrySecret. 
+*/
+
+def giveRegistryAccessToNamespace (String namespace, String registrySecret) { 
+  String secretScript = "kubectl get secret/${registrySecret} -o jsonpath=\"{.data.\\.dockercfg}\""
+  String secret = sh (script: secretScript, returnStdout: true).trim()
+  String yaml = """
+  apiVersion: v1
+  data:
+    .dockercfg: ${secret}
+  kind: Secret
+  metadata:
+    name: ${registrySecret}
+  type: kubernetes.io/dockercfg
+  """
+  sh "printf -- \"${yaml}\" | kubectl apply --namespace ${namespace} -f -"
+
+  String sa = sh (script: "kubectl get sa default -o json --namespace ${namespace}", returnStdout: true).trim()
+  /*
+      JsonSlurper is not thread safe, not serializable, and not good to use in Jenkins jobs. See 
+      https://stackoverflow.com/questions/37864542/jenkins-pipeline-notserializableexception-groovy-json-internal-lazymap
+  */
+  def map = new JsonSlurperClassic().parseText (sa) 
+  map.metadata.remove ('resourceVersion')
+  map.put ('imagePullSecrets', [['name': registrySecret]])
+  def json = JsonOutput.prettyPrint(JsonOutput.toJson(map))
+  writeFile file: 'temp.json', text: json
+  sh "kubectl replace sa default --namespace ${namespace} -f temp.json"
 }
