9aec047accc0c61325bbd06972615fbddc68b1f5
Mindaugas <mindaugas.raudavicius@gmail.com>
2017-10-24 13:31:18 +0100

    admin.registrykey migration to testing namespaces (#46)
    * Added a simplified secret migration shell command.
    
    The command contains jq, which needs testing as it might not be
    installed on the image within the pipeline.
    
    * Simplified service account patching.
    
    * Changed secret migration bit to only migrate one required secret.
    
    * Modified giveRegistryAccessToNamespace function
    
    1) Removed legacy code
    2) Removed extra parameter
    3) Updated the comment.
    
    * Changend the comment.
    
    * Replaced jq with sed as jq is not available in kubectl image yet.
    
    * Moved sh command to one line.

--- vars/microserviceBuilderPipeline.groovy
+++ vars/microserviceBuilderPipeline.groovy
@@ -176,7 +176,7 @@ def call(body) {
             sh "kubectl create namespace ${testNamespace}"
             sh "kubectl label namespace ${testNamespace} test=true"
             if (registrySecret) {
-              giveRegistryAccessToNamespace (testNamespace, registrySecret)
+              giveRegistryAccessToNamespace (testNamespace)
             }
           }
           // We're moving to Helm-only deployments. Use Helm to install a deployment to test against.
@@ -242,39 +242,16 @@ def deployProject (String chartFolder, String registry, String image, String ima
 }
 
 /*
-  We have a (temporary) namespace that we want to grant CfC registry access to.
+  We have a (temporary) namespace that we want to grant ICP registry access to.
   String namespace: target namespace
-  String registrySecret: secret in Jenkins' namespace to use
 
-  1. Port registrySecret into namespace
+  1. Port registrySecret into a temporary namespace
   2. Modify 'default' serviceaccount to use ported registrySecret.
 */
 
-def giveRegistryAccessToNamespace (String namespace, String registrySecret) {
-  String secretScript = "kubectl get secret/${registrySecret} -o jsonpath=\"{.data.\\.dockercfg}\""
-  String secret = sh (script: secretScript, returnStdout: true).trim()
-  String yaml = """
-  apiVersion: v1
-  data:
-    .dockercfg: ${secret}
-  kind: Secret
-  metadata:
-    name: ${registrySecret}
-  type: kubernetes.io/dockercfg
-  """
-  sh "printf -- \"${yaml}\" | kubectl apply --namespace ${namespace} -f -"
-
-  String sa = sh (script: "kubectl get sa default -o json --namespace ${namespace}", returnStdout: true).trim()
-  /*
-      Use JsonSlurperClassic because JsonSlurper is not thread safe, not serializable, and not good to use in Jenkins jobs.
-      See https://stackoverflow.com/questions/37864542/jenkins-pipeline-notserializableexception-groovy-json-internal-lazymap
-  */
-  def map = new JsonSlurperClassic().parseText (sa)
-  map.metadata.remove ('resourceVersion')
-  map.put ('imagePullSecrets', [['name': registrySecret]])
-  def json = JsonOutput.prettyPrint(JsonOutput.toJson(map))
-  writeFile file: 'temp.json', text: json
-  sh "kubectl replace sa default --namespace ${namespace} -f temp.json"
+def giveRegistryAccessToNamespace (String namespace) {
+  sh "kubectl get secret admin.registrykey -o json --namespace default | sed 's/\"namespace\": \"default\"/\"namespace\": \"${namespace}\"/g' | kubectl create -f -"
+  sh "kubectl patch serviceaccount default -p '{\"imagePullSecrets\": [{\"name\": \"myregistrykey\"}]}' --namespace ${namespace}"
 }
 
 def getChartFolder(String userSpecified, String currentChartFolder) {
