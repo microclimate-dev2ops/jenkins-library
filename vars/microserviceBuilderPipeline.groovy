9437e260e2cc7ce22f670f3c948359bee23c7e87
Adam Roberts <aroberts@uk.ibm.com>
2018-04-12 14:16:07 +0100

    Update microserviceBuilderPipeline.groovy

--- vars/microserviceBuilderPipeline.groovy
+++ vars/microserviceBuilderPipeline.groovy
@@ -74,6 +74,10 @@ def call(body) {
   def alwaysPullImage = (env.ALWAYS_PULL_IMAGE == null) ? true : env.ALWAYS_PULL_IMAGE.toBoolean()
   def mavenSettingsConfigMap = env.MAVEN_SETTINGS_CONFIG_MAP?.trim()
 
+  node {
+    echo sh(returnStdout: true, script: 'env')
+  }
+  
   print "microserviceBuilderPipeline: registry=${registry} registrySecret=${registrySecret} build=${build} \
   deploy=${deploy} test=${test} debug=${debug} namespace=${namespace} tillerNamespace=${tillerNamespace} \
   chartFolder=${chartFolder} manifestFolder=${manifestFolder} alwaysPullImage=${alwaysPullImage} serviceAccountName=${serviceAccountName}"
