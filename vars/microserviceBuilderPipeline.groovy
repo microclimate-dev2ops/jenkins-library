2b91a8de8f35175638b0e8c79a41377e7a5eda75
Adam Roberts <aroberts@uk.ibm.com>
2018-10-08 10:43:52 +0100

    More fun

--- vars/microserviceBuilderPipeline.groovy
+++ vars/microserviceBuilderPipeline.groovy
@@ -140,7 +140,7 @@ def call(body) {
       stage ('Extract') {
 	  // E.g. may wish to add http.sslVerify false when working with one's own SCM that has self-signed certs
 	  if (extraGitOptions) {
-	    echo "Extra git options found, setting global Git options to include ${extraGitOptions}"
+	    echo "Extra Git options found, setting global Git options to include ${extraGitOptions}"
 	    configSet = sh(script: 'git config ${extraGitOptions}', returnStdout: true)
           }
 	  checkout scm
